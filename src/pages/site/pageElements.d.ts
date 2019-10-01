2253f074222decc0fb56d6e7b599fce41b64026e
Mor <morg@wix.com>
2019-10-01 14:50:12 +0300

    remove uneeded files

--- src/pages/site/pageElements.d.ts
+++
@@ -1,9 +0,0 @@
-type PageElementsMap = {
-  "#header1": $w.Header;
-  "#footer1": $w.Footer;
-  "#line4": $w.HiddenCollapsedElement;
-  "#group2": $w.Box;
-  "#image3": $w.Image;
-  "#line2": $w.HiddenCollapsedElement;
-  "#text4": $w.Text;
-}
