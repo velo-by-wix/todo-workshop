c5b5331db2b101e4a1f91aa4be3896c0de9ab731
Anthony Lusardi <92751614+anthony-wix@users.noreply.github.com>
2023-01-06 15:02:43 -0600

    update source for the revised example

--- src/lightboxes/Clear_Confirmation.m62jr/Clear_Confirmation.js
+++ src/lightboxes/Clear_Confirmation.m62jr/Clear_Confirmation.js
@@ -1,13 +1,13 @@
 import wixWindow from 'wix-window';
 
 $w.onReady(function () {
-	//TODO: write your page related code here...
+
+    $w('#clearButton').onClick(() => {
+        wixWindow.lightbox.close(true);
+    });
+
+    $w('#cancelButton').onClick(() => {
+        wixWindow.lightbox.close(false);
+    });
+
 });
-
-export function approveBtn_click(event) {
-	wixWindow.lightbox.close(true)
-}
-
-export function rejectBtn_click(event) {
-	wixWindow.lightbox.close(false)
-}
--- src/pages/HOME.c1dmp/HOME.js
+++ src/pages/HOME.c1dmp/HOME.js
@@ -1,96 +1,137 @@
 import wixData from 'wix-data';
 import wixWindow from 'wix-window';
-import { getActiveTaskCount, removeCompletedTasks } from 'backend/taskOperations'
+
+import { authentication } from 'wix-members';
+import { getQuote } from "backend/motivation.jsw";
+
+const TODO_COLLECTION = 'TodoTasks'
+const TODO_DATASET = '#dataset1';
 
 $w.onReady(async function () {
-	await updateActiveTaskCount()
-	const addNewTask = async () => {
-		const taskTitle = $w('#taskInput').value
+    // Event handlers for adding a task
+    $w('#addTaskButton').onClick(addNewTask);
 
-		if (taskTitle.trim() === '') {
-			return
-		}
+    $w('#taskInput').onKeyPress(async (event) => {
+        const { key } = event;
 
-		const newTask = {
-			title: taskTitle,
-			completed: false
-		}
+        if (key === 'Enter') addNewTask();
+    });
 
-		await wixData.insert('TodoTasks', newTask)
-		await $w('#dataset1').refresh()
-		$w('#taskInput').value = ''
-		await Promise.all([updateActiveTaskCount(), filterTasks()])
-	}
-	$w('#addTaskButton').onClick(async () => {
-		await addNewTask()
-	})
+    // Event handler for updating dataset filter
+    $w('#filterRadioGroup').onChange((event) => setDatasetFilter(event.target.value));
 
-	$w('#taskInput').onKeyPress(async event => {
-		const { key } = event
-		if (key === 'Enter') {
-			await addNewTask()
-		}
-	})
+    // Event handler for when a todo's completed status is changed
+    $w('#completedSwitch').onChange(async (event) => {
+        await handleSwitchChange(event);
 
-	$w('#completedCheckbox').onChange(async (event) => {
-		const checked = event.target.checked
-		const itemId = event.context.itemId
+        getIncompleteTodoCount();
 
-		const item = await wixData.get('TodoTasks', itemId)
-		const updatedItem = Object.assign({}, item, { completed: checked })
-		await wixData.update('TodoTasks', updatedItem)
-		await Promise.all([updateActiveTaskCount(), filterTasks()])
-	})
+        $w(TODO_DATASET).refresh();
+    });
 
-	$w('#filterRadioGroup').onChange(async () => {
-		await filterTasks()
-	})
+    // Event handler for when a user wants to clear completed tasks
+    $w('#clearCompletedButton').onClick(async () => {
+        let confirmed = await wixWindow.openLightbox('Clear Confirmation');
 
-	$w('#clearCompletedButton').onClick(async () => {
-		const shouldClearTasks = await wixWindow.openLightbox('Clear Confirmation')
+        if (confirmed) {
+            clearCompletedTasks();
+        }
+    })
 
-		if (shouldClearTasks) {
-			await removeCompletedTasks()
-			await $w('#dataset1').refresh()
-		}
-	})
+    // Setup the page/init on both page render and login
+    // Login doesn't trigger a new page render
+    authentication.onLogin(init)
+    init();
 });
 
-async function filterTasks() {
-	const filterValue = $w('#filterRadioGroup').value
-
-	let filter
-
-	switch (filterValue) {
-	case 'Completed':
-		filter = wixData.filter().eq('completed', true)
-		break;
-	case 'Active':
-		filter = wixData.filter().eq('completed', false)
-		break;
-	case 'All Tasks':
-		filter = wixData.filter()
-		break;
-	}
-
-	await $w('#dataset1').setFilter(filter)
+// Initialize the page with data that we need
+// but don't get through the dataset
+async function init() {
+    getIncompleteTodoCount();
+    if (wixWindow.rendering.env === 'browser') {
+        $w('#tagline').text = await getQuote();
+        $w('#tagline').show("float");
+    }
 }
 
-async function updateActiveTaskCount() {
-	const activeTaskCount = await getActiveTaskCount()
+// Handle adding a new task
+async function addNewTask() {
+    const taskTitle = $w('#taskInput').value;
+    if (taskTitle.trim() === '') {
+        return;
+    }
 
-	let activeTaskText
-	switch (activeTaskCount) {
-	case 0:
-		activeTaskText = 'Completed all tasks'
-		break;
-	case 1:
-		activeTaskText = '1 item left'
-		break;
-	default:
-		activeTaskText = `${activeTaskCount} items left`
-		break;
-	}
+    const newTask = {
+        title: taskTitle,
+        completed: false
+    }
 
-	$w('#activeTasksCount').text = activeTaskText
+    $w('#taskInput').disable();
+
+    await wixData.insert(TODO_COLLECTION, newTask);
+    $w(TODO_DATASET).refresh();
+
+    $w('#taskInput').value = "";
+    $w('#taskInput').enable();
+}
+
+// Update the collection when a todo's completed status is changed
+async function handleSwitchChange(event) {
+    const $item = $w.at(event.context);
+    const _id = event.context.itemId;
+    const title = $item('#taskText').text;
+    const checked = $item('#completedSwitch').checked;
+
+    let updatedItem = {
+        _id,
+        title,
+        completed: checked
+    }
+
+    return wixData.update(TODO_COLLECTION, updatedItem);
+}
+
+// Get the count of TODOs that are not marked completed
+function getIncompleteTodoCount() {
+    wixData
+        .query(TODO_COLLECTION)
+        .eq('completed', false)
+        .count({ consistentRead: true }) // Count from primary database, changes may not have propagated by the time we count()
+        .then((result) => {
+            $w('#activeTasksCount').text = `${result} items left`;
+            $w('#activeTasksCount').show("float");
+        })
+        .catch(() => {
+            $w('#activeTasksCount').hide();
+        })
+}
+
+// Filter the dataset based on the filter value selected
+function setDatasetFilter(filterValue) {
+    let filter;
+
+    switch (filterValue) {
+    case 'Completed':
+        filter = wixData.filter().eq('completed', true);
+        break;
+    case 'Active':
+        filter = wixData.filter().eq('completed', false);
+        break;
+    default:
+        filter = wixData.filter();
+        break;
+    }
+
+    $w(TODO_DATASET).setFilter(filter);
+}
+
+// 
+async function clearCompletedTasks() {
+    let completed = await wixData.query(TODO_COLLECTION)
+        .eq('completed', true)
+        .find()
+    let toRemove = completed.items.map(i => i._id);
+    await wixData.bulkRemove(TODO_COLLECTION, toRemove);
+
+    $w('#dataset1').refresh();
 }
